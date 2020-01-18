24d1bf2efd434c26e948a5502fcf6257a8ddd9e7
shyamtopiya <shyamtopiya2652@gmail.com>
2020-01-18 18:05:05 +0530

    learning echo statement

---
+++ 18-01-2020/echo.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$name = "shyam topiya";
+$firstnumber = 2;
+$lastnumber = 6;
+
+echo "Hello World!!";
+echo "<br>using html tag in echo<br>";
+echo $name."<br>";
+echo $firstnumber + $lastnumber;
+echo "<br>";
+
+?>
+
+<input type="text" value="<?php echo $name; ?>">
