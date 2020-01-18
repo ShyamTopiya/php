b5b414939fa518caa0ed9d42ddaba0fa1beafc2d
shyamtopiya <shyamtopiya2652@gmail.com>
2020-01-18 18:13:48 +0530

    learning print statement

---
+++ 18-01-2020/print.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Shyam";
+$surname = "Topiya";
+$value1 = 5;
+$value2 = 0.6;
+
+print("Hello World!!!");
+print "<h3>hiii ".$name."</h3>";
+print "addition is".($value1+$value2)."";
+print '<p>using HTML tags</p>';
+
+
+?>
