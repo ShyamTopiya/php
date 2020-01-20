b984cd2910592220790f03a6f77ca6f3f04fc8df
shyamtopiya <shyamtopiya2652@gmail.com>
2020-01-20 18:00:42 +0530

    learning stringFunctions

---
+++ 20-01-2020/stringFunctions.php
@@ -0,0 +1,22 @@
+<?php
+
+$string = "My Name Is Shyam Topiya & I Am Engineer";
+
+$string_word_count = str_word_count($string,2,"&");
+
+$string_suffled = str_shuffle($string);
+
+$sub_string = substr($string_suffled,0,10); 
+
+$string_reversed = strrev($string);
+
+$string_length = strlen($string); 
+
+print_r($string_word_count);
+echo "<br><br>".$string_suffled;
+echo "<br><br>subString is: ".$sub_string;
+echo "<br><br>Reversed String Is: ".$string_reversed;
+echo "<br><br>".$string_length."<br><br>";
+echo trim($string,"Myineer");
+
+?>
