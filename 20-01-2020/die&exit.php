65fa5b11be01c5fa747eb84118574e621ffcefbe
shyamtopiya <shyamtopiya2652@gmail.com>
2020-01-20 10:15:07 +0530

    learning die and exit functon

---
+++ 20-01-2020/die&exit.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello Shyam";
+
+
+echo "<br><br>";
+echo "Hello";
+
+die("     .PAGE HAS ENDED");
+
+echo "Shyam";
+
+//--------------------Same As Die For Ending The Page--------------------------------
+
+echo "good";
+
+exit();
+
+echo "morning";
