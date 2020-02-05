f66985c83647d7eef16c0e5520a4a780c9476a24
shyamtopiya <shyamtopiya2652@gmail.com>
2020-02-05 11:26:25 +0530

    exam

--- phpExam/blog.sql
+++ phpExam/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 03, 2020 at 01:12 PM
+-- Generation Time: Feb 05, 2020 at 06:54 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -30,15 +30,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `blog_post` (
   `blog_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `parent_name` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
   `url` varchar(150) NOT NULL,
   `content` varchar(100) NOT NULL,
-  `image` varchar(150) NOT NULL,
+  `categoryImage` varchar(150) NOT NULL,
   `published_at` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `blog_post`
+--
+
+INSERT INTO `blog_post` (`blog_id`, `parent_name`, `title`, `url`, `content`, `categoryImage`, `published_at`, `created_at`, `updated_at`) VALUES
+(12, 'Health,Music', 'shyam', 'jjkjk', 'hjhjh', 'uploads/NATURE+GENERIC+TREES.jpg', '2020-01-01', '2020-02-04 12:52:49', '2020-02-04 12:52:49'),
+(13, 'LifeStyle,Health', 'rohit', 'qwer', 'qwe', 'uploads/', '2020-01-01', '2020-02-04 12:54:35', '2020-02-04 12:54:35');
 
 -- --------------------------------------------------------
 
@@ -49,26 +57,45 @@ CREATE TABLE `blog_post` (
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
   `parent_category_id` int(11) NOT NULL,
+  `parent_name` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
   `metaTitle` varchar(100) NOT NULL,
   `url` varchar(100) NOT NULL,
   `content` varchar(150) NOT NULL,
+  `categoryImage` varchar(500) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `parent_category_id`, `parent_name`, `title`, `metaTitle`, `url`, `content`, `categoryImage`, `created_at`, `updated_at`) VALUES
+(9, 1, 'LifeStyle', 'hah', 'jkjk', 'jjkjk', 'jkjk', 'uploads/NATURE+GENERIC+TREES.jpg', '2020-02-04 11:18:37', '2020-02-04 11:18:37');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `parentcagory`
+-- Table structure for table `parentcategory`
 --
 
-CREATE TABLE `parentcagory` (
+CREATE TABLE `parentcategory` (
   `parent_id` int(11) NOT NULL,
   `title` varchar(200) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `parentcategory`
+--
+
+INSERT INTO `parentcategory` (`parent_id`, `title`, `created_at`, `updated_at`) VALUES
+(1, 'LifeStyle', '2020-02-04 07:48:43', '2020-02-04 07:48:43'),
+(2, 'Health', '2020-02-04 07:48:43', '2020-02-04 07:48:43'),
+(3, 'Education', '2020-02-04 07:48:43', '2020-02-04 07:48:43'),
+(4, 'Music', '2020-02-04 07:48:43', '2020-02-04 07:48:43');
 
 -- --------------------------------------------------------
 
@@ -95,7 +122,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `prefix`, `firstName`, `lastName`, `mobileNumber`, `email`, `password`, `lastLogIn`, `information`, `createdAt`, `updatedAt`) VALUES
-(1, 'Mr', 'shyam', 'topiya', 8866535619, 'shyam@gmail.com', '123', '2020-02-03 09:50:39', 'hii', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'Mr', 'shyam', 'topiya11', 8866535619, 'shyam@gmail.com', '123', '2020-02-04 09:42:07', 'hii', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Mr', 'qq', 'll', 8855221144, 'sg@gmail.com', '123', '2020-02-04 05:51:31', 'qq', '2020-02-04 05:51:31', '2020-02-04 05:51:31'),
+(3, 'Mr', 'keyur', 'patel', 1212121212, 'aa@gmail.com', '123', '2020-02-04 05:54:19', 'qq', '2020-02-04 05:54:19', '2020-02-04 05:54:19'),
+(4, 'Mr', 'shyam', 'topiya', 8866535619, 'sam@gmail.com', '123', '2020-02-04 06:11:11', 'qq', '2020-02-04 06:11:11', '2020-02-04 06:11:11');
 
 --
 -- Indexes for dumped tables
@@ -105,8 +135,7 @@ INSERT INTO `user` (`user_id`, `prefix`, `firstName`, `lastName`, `mobileNumber`
 -- Indexes for table `blog_post`
 --
 ALTER TABLE `blog_post`
-  ADD PRIMARY KEY (`blog_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`blog_id`);
 
 --
 -- Indexes for table `category`
@@ -116,9 +145,9 @@ ALTER TABLE `category`
   ADD KEY `category_ibfk_1` (`parent_category_id`);
 
 --
--- Indexes for table `parentcagory`
+-- Indexes for table `parentcategory`
 --
-ALTER TABLE `parentcagory`
+ALTER TABLE `parentcategory`
   ADD PRIMARY KEY (`parent_id`);
 
 --
@@ -135,41 +164,35 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `blog_post`
 --
 ALTER TABLE `blog_post`
-  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `parentcagory`
+-- AUTO_INCREMENT for table `parentcategory`
 --
-ALTER TABLE `parentcagory`
-  MODIFY `parent_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `parentcategory`
+  MODIFY `parent_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `blog_post`
---
-ALTER TABLE `blog_post`
-  ADD CONSTRAINT `blog_post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `category`
 --
 ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_category_id`) REFERENCES `parentcagory` (`parent_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_category_id`) REFERENCES `parentcategory` (`parent_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
